6d6fb24008598b1cb3501ca9df9465e0d9ce41c0
Bayan2019 <saparbayevabt@gmail.com>
2023-12-06 13:09:46 -0800

    clean input

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"os"
+// )
 
 func main() {
-	fmt.Println("Hello World")
+	// fmt.Println("Hello World")
+	startRepl()
 }
---
+++ repl.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Println("pokedex >")
+
+		scanner.Scan()
+		text := scanner.Text()
+
+		cleaned := cleanInput(text)
+		if len(cleaned) == 0 {
+			continue
+		}
+
+		fmt.Println("echoing: ", cleaned)
+	}
+
+}
+
+func cleanInput(str string) []string {
+	lowered := strings.ToLower(str)
+	words := strings.Fields(lowered)
+	return words
+}
